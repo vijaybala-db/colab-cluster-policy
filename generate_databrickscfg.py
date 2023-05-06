6cad120d6d5b6265b6807957068f80a68330a678
Vijay Balasubramaniam <vbalasu@gmail.com>
2023-05-06 09:40:30 -0700

    Added option to generate workspace list or databrickscfg

--- generate_databrickscfg.py
+++ generate_databrickscfg.py
@@ -5,6 +5,10 @@ def get_workspaces():
     df = spreadsheet.sheet_to_df(sheet='Workspaces')
     return df.to_dict(orient='records')
 
+def print_workspace_names(workspaces):
+    workspace_names = [workspace['Endpoint'].replace('enb-', '').replace('.cloud.databricks.com', '') for workspace in workspaces]
+    print('\n'.join(workspace_names))
+
 def decrypt(encrypted_token):
     from dotenv import load_dotenv
     from cryptography.fernet import Fernet
@@ -14,7 +18,7 @@ def decrypt(encrypted_token):
     f = Fernet(key)
     return f.decrypt(encrypted_token)
 
-def get_databrickscfg(workspaces):
+def print_databrickscfg(workspaces):
     import configparser, sys
     config = configparser.ConfigParser()
     for workspace in workspaces:
@@ -26,4 +30,16 @@ def get_databrickscfg(workspaces):
         }
     config.write(sys.stdout)
 
-get_databrickscfg(get_workspaces())
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--list', action="store_true", help="List workspace names")
+parser.add_argument('-d', '--databrickscfg', action="store_true", help="Generate Databricks config for CLI")
+args = parser.parse_args()
+if args.list:
+    workspaces = get_workspaces()
+    print_workspace_names(workspaces)
+elif args.databrickscfg:
+    workspaces = get_workspaces()
+    print_databrickscfg(workspaces)
+else:
+    parser.print_help()
